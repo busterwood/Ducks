d78edf24e63dacd670bf786c9db850727fb021bf
busterwood <busterwood@users.noreply.github.com>
2016-11-21 10:56:50 +0000

    initial code before Sigil

---
+++ Ducks/Duck.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Threading;
+
+namespace Ducks
+{
+    public static class Duck
+    {
+        static readonly ConcurrentDictionary<TypePair, Func<object, object>> casts = new ConcurrentDictionary<TypePair, Func<object, object>>();
+
+        public static T Cast<T>(object obj) where T : class
+        {
+
+            var item = obj as T;
+            if (item != null)
+                return item;
+
+            return (T)Cast(obj, typeof(T));
+        }
+
+        static object Cast(object from, Type to)
+        {
+            if (from == null)
+                throw new ArgumentNullException(nameof(from));
+            if (to == null)
+                throw new ArgumentNullException(nameof(to));
+
+            var func = casts.GetOrAdd(new TypePair(from.GetType(), to), pair => CreateProxy(pair.From, pair.To));
+            return func(from);
+        }
+
+        /// <param name="duck">The duck</param>
+        /// <param name="interface">the interface to cast <paramref name="duck"/></param>
+        static Func<object, object> CreateProxy(Type duck, Type @interface)
+        {
+            if (duck == null)
+                throw new ArgumentNullException(nameof(duck));
+            if (@interface == null)
+                throw new ArgumentNullException(nameof(@interface));
+            if (!@interface.IsInterface)
+                throw new ArgumentException($"{@interface} is not an interface");
+
+            AppDomain domain = Thread.GetDomain();
+            string assemblyName = "Ducks_Proxy_" + @interface.AsmName() + "_" + duck.AsmName() + ".dll";
+            var assemblyBuilder = domain.DefineDynamicAssembly(new AssemblyName(assemblyName), AssemblyBuilderAccess.Run);
+            var moduleBuilder = assemblyBuilder.DefineDynamicModule(assemblyName);
+            var typeBuilder = moduleBuilder.DefineType("Proxy");
+            typeBuilder.AddInterfaceImplementation(@interface);
+
+
+        }
+
+        static string AsmName(this Type type) => type.Name.Replace(".", "_").Replace("+", "-");
+    }
+}
---
+++ Ducks/TypePair.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ducks
+{
+
+    struct TypePair : IEquatable<TypePair>
+    {
+        public readonly Type From;
+        public readonly Type To;
+
+        public TypePair(Type from, Type to)
+        {
+            if (from == null)
+                throw new ArgumentNullException(nameof(from));
+            if (to == null)
+                throw new ArgumentNullException(nameof(to));
+            From = from;
+            To = to;
+        }
+
+        public bool Equals(TypePair other) => Equals(From, other.From) && Equals(To, other.To);
+
+        public override bool Equals(object obj) => obj is TypePair ? Equals((TypePair)obj) : false;
+
+        public override int GetHashCode() => From == null ? 0 : From.GetHashCode() + To.GetHashCode();
+    }
+}
